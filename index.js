fe615dabab4513b16d38141d497966a8fe5ebebc
cdojeda <cristianojedamaimo@gmail.com>
2018-06-06 20:43:25 -0300

    Cambios rutas
    Agregando rutas y middlewares

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function(){
-    console.log('Servidor funcionando');
+
+// Middlewares
+app.use(function(req,res,next){
+    console.log("request url: "+req.url);
+    next();
 });
 
-app.get('/', function(req,res){
-    res.end("ola Mundo");
+app.use( (req,res,next) => {
+    console.log("2do middleware");
+    next();
 })
+
+app.get('/', function(req,res){
+    res.send("hola mundo");
+});
+
+app.get('/login', function(req,res){
+    res.send("aqui va el login");
+});
+
+app.get('*', (req,res) => {
+    res.end("Archivo no encontrado");
+});
+
+app.listen(3000, function(){
+    console.log("Servidor funcionando");
+});
